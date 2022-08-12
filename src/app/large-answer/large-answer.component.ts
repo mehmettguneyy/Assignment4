4b9dbdb63499dc365121a662779aca76eb168315
Mehmet GUNEY <guneym@sheridancollege.ca>
2022-08-12 18:16:56 -0400

    Added html for large-answer.component.ts and new-problem.component.ts.

--- src/app/large-answer/large-answer.component.ts
+++ src/app/large-answer/large-answer.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {GameQuizService} from '../game-quiz.service';
+import {GameProblem} from '../game-problem';
 
 @Component({
   selector: 'app-large-answer',
   templateUrl: './large-answer.component.html',
-  styleUrls: ['./large-answer.component.css']
+  styleUrls: ['./large-answer.component.css', '../../assets/css/main.css']
 })
-export class LargeAnswerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class LargeAnswerComponent {
+  problem: GameProblem;
+  userAnswer: string;
+  constructor(quiz: GameQuizService) {
+    this.problem = quiz.problem;
+    this.userAnswer = quiz.userAnswer;
   }
-
 }
--- src/app/new-problem/new-problem.component.ts
+++ src/app/new-problem/new-problem.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {GameProblem} from '../game-problem';
+import {GameQuizService} from '../game-quiz.service';
+import {navigate} from '../navigate';
 
 @Component({
   selector: 'app-new-problem',
   templateUrl: './new-problem.component.html',
-  styleUrls: ['./new-problem.component.css']
+  styleUrls: ['./new-problem.component.css', '../../assets/css/main.css']
 })
-export class NewProblemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class NewProblemComponent {
+  problem: GameProblem;
+  constructor(
+    private quiz: GameQuizService,
+    private readonly router: Router
+  ) {
+    this.problem = quiz.problem;
+    this.problem.reset();
   }
 
+  public check(userAnswer: string) {
+    this.quiz.userAnswer = userAnswer;
+    console.log('userAnswer=[' + userAnswer + ']');
+    navigate(this.quiz.gradeQuiz(), this.router);
+  }
 }
+
+
