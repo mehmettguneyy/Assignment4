623c8bfe2b0136b5c923096a169c89ca2b8edaaa
Mehmet GUNEY <guneym@sheridancollege.ca>
2022-08-12 18:32:05 -0400

    Changed minor style issues

--- src/app/try-again/try-again.component.ts
+++ src/app/try-again/try-again.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {GameProblem} from '../game-problem';
+import {GameQuizService} from '../game-quiz.service';
+import {navigate} from '../navigate';
 
 @Component({
   selector: 'app-try-again',
   templateUrl: './try-again.component.html',
-  styleUrls: ['./try-again.component.css']
+  styleUrls: ['./try-again.component.css', '../../assets/css/main.css']
 })
-export class TryAgainComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class TryAgainComponent {
+  problem: GameProblem;
+  constructor(
+    private quiz: GameQuizService,
+    private readonly router: Router) {
+    this.problem = quiz.problem;
   }
-
+  public check(userAnswer: string) {
+    this.quiz.userAnswer = userAnswer;
+    console.log('userAnswer=[' + userAnswer + ']');
+    navigate(this.quiz.gradeQuiz(), this.router);
+  }
 }
+
